d051a9ed7d5ed93010e8290344ac858f04b8d8ce
Maxime <47356381+CSharper63@users.noreply.github.com>
2024-01-10 22:17:03 +0100

    test on main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 pub mod crypto;
 pub mod endpoints;
 pub mod model;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let password = "password";
+    let username = "john";
+
+    // call the api to register the user
+    let (jwt, clear_user) = endpoints::sign_up(username, password).await;
+
+    println!(
+        "{}/{}",
+        jwt.unwrap().clone(),
+        bs58::encode(clear_user.clone().unwrap().master_key.asset.unwrap()).into_string()
+    );
+
+    let enc_user = clear_user.clone().unwrap().encrypt();
 }
